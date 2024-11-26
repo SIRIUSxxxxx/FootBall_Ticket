b107abea0b34b61beeb0f7def6cb53661b912085
Foon323 <131373130+Foon323@users.noreply.github.com>
2024-11-26 15:12:39 +0800

    Update reportWebVitals.js

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+//22031515D Fok Luk Hang
+//22026938D Poon Cheuk Kit
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
