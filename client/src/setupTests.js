9147e4aaf3f6a687039ea10d61f76975e34a1ebc
Foon323 <131373130+Foon323@users.noreply.github.com>
2024-11-26 15:12:57 +0800

    Update setupTests.js

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,3 +1,5 @@
+//22031515D Fok Luk Hang
+//22026938D Poon Cheuk Kit
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
